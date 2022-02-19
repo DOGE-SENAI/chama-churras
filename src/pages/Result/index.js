baf02876dd3e06b5831de5ff56ae29585046b6e5
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2022-02-19 15:33:51 -0300

    Página resultado

--- src/pages/Result/index.js
+++ src/pages/Result/index.js
@@ -1,6 +1,13 @@
-import { StyleSheet, View, Text, ScrollView } from "react-native";
+import {
+	StyleSheet,
+	View,
+	Text,
+	ScrollView,
+	TouchableOpacity,
+} from "react-native";
 import React from "react";
 import Header from "../../components/Header";
+import FriendsIllutration from "../../assets/images/friends.svg";
 
 const Result = () => {
 	return (
@@ -9,6 +16,28 @@ const Result = () => {
 
 			<View style={styles.containerMain}>
 				<Text style={styles.title}>Resultado</Text>
+
+				<View style={styles.content}>
+					<Text style={styles.textContent}>
+						Churrasco calculado! Tenha um bom churrasco.
+					</Text>
+
+					<View style={styles.imageFriends}>
+						<FriendsIllutration />
+					</View>
+
+					<View style={styles.containerBtn}>
+						<TouchableOpacity style={styles.btn}>
+							<Text style={styles.btnText}>Compartilhar Resultado</Text>
+						</TouchableOpacity>
+
+						<Text style={styles.legendBtn}>
+							Para obter o resultado você precisa clicar em "Compartilhar" e
+							salvar o PDF. Você também pode compartilhá-lo em outras
+							plataformas.
+						</Text>
+					</View>
+				</View>
 			</View>
 		</ScrollView>
 	);
@@ -25,14 +54,48 @@ const styles = StyleSheet.create({
 		flex: 1,
 		backgroundColor: "#E6E8E1",
 		alignItems: "center",
+		width: "100%",
 	},
-	result: {
-		width: "80%",
-		marginTop: 80,
-		flexDirection: "row",
+	content: {
+		marginTop: 30,
 		alignItems: "center",
-		justifyContent: "space-around",
 	},
+	textContent: {
+		fontSize: 20,
+		margin: 16,
+		textAlign: "center",
+		fontWeight: "700",
+	},
+	imageFriends: {
+		alignItems: "center",
+		justifyContent: "center",
+	},
+
+	containerBtn: {
+		margin: 20,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	btn: {
+		borderRadius: 50,
+		height: 60,
+		width: 260,
+		marginBottom: 23,
+		alignItems: "center",
+		justifyContent: "center",
+		backgroundColor: "#833116",
+	},
+	btnText: {
+		fontSize: 20,
+		color: "#fafafa",
+		textAlign: "center",
+	},
+	legendBtn: {
+		textAlign: "center",
+		fontSize: 16,
+		fontWeight: "400",
+	},
+
 	scrollContainer: {
 		backgroundColor: "#E6E8E1",
 	},
